28ac6391c0273fe5bb5e60e8ae7182a2e75eb3c2
mlintelman <michaellintelman03@gmail.com>
2025-08-11 15:39:21 -0400

    VS race page, basic
    Basic functionality on VS Race page. Need to add race result logging. Also added env file for keys and urls.

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -4,7 +4,7 @@ import Footer from "./Footer";
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-100">
+    <div className="min-h-screen flex flex-col bg-blue-400">
       <Navbar />
 
       <main className="flex-1 max-w-5xl mx-auto w-full p-4 bg-white shadow-lg rounded-lg">
--- frontend/src/pages/VS.jsx
+++ frontend/src/pages/VS.jsx
@@ -2,13 +2,70 @@ import React, { useState, useEffect } from "react";
 import { locations } from "../data/data";
 
 export default function VS() {
+    const [numRaces, setNumRaces] = useState("");
+    const [randomizedLocations, setRandomizedLocations] = useState([])
 
+    function randomizeLocations(arr, n) {
+        const shuffledLocations = [...arr]
+
+        // Use the fisher-yates shuffle
+        for (let i = shuffledLocations.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledLocations[i], shuffledLocations[j]] = [shuffledLocations[j], shuffledLocations[i]]
+        }
+        return setRandomizedLocations(shuffledLocations.slice(0,n))
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault(); // prevent page refresh
+        console.log("Number of races:", numRaces);
+        randomizeLocations(locations, numRaces)
+    }
+
+    useEffect(() => {
+        console.log("randomized: ", randomizedLocations)
+    }, [randomizedLocations])
+    
     return (
-        <div className="justify-center">
+        <div className="justify-center font-medium text-gray-800 border-gray-800">
             <p className="text-2xl font-semibold">VS Race Randomizer</p>
-            <hr className="mb-4"></hr>
-            <label className="text-xl">Enter the number of races: </label>
-            <input id="numRacesInput" type="tel" className="text-xl rounded-sm border-2 max-w-10"/>
+            <hr className="mb-4" />
+
+            {/* Form for entering the number of races */}
+            <form onSubmit={handleSubmit}>
+                <label className="text-xl">
+                    Enter the number of races:{" "}
+                    <input
+                        id="numRacesInput"
+                        type="tel"
+                        value={numRaces}
+                        onChange={(e) => setNumRaces(e.target.value)}
+                        className="text-xl rounded-sm border-2 max-w-10"
+                    />
+                </label>
+                <br />
+                <button
+                    type="submit"
+                    className="text-xl bg-blue-500 text-white rounded-md pl-2 pr-2 pt-1 pb-1 mt-2"
+                >
+                    Submit
+                </button>
+            </form>
+            <hr className="mt-4 mb-4" />
+
+            {/* Display the races for the user */}
+            <div className="block">
+                {randomizedLocations.map((location, index) => (
+                    <p key={index} className="text-lg font-medium">
+                        Race {index + 1}: {location.name}
+                    </p>
+                ))}
+            </div>
+
+            {/* VS Race results */}
+            <div>
+                
+            </div>
         </div>
-    )
+    );
 }
